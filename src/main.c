6b3aee2658458e343dd74e7e67023270b560a09b
jaduksuh <jaduksuh>
2025-01-24 21:01:24 +0900

    Add SDL window functionality

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
 #include "register.h"
 #include "graphics.h"
 
-int main()
+int main(int argc, char** argv)
 {
+    SDL_Window* window;
+
+    /* Initialize SDL */
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS);
+
     /* Configure chip-8 components */
     char* memory = (char*)malloc(4096);
     register_t* registers = (register_t*)malloc(sizeof(register_t));
@@ -16,6 +24,39 @@ int main()
 
     setup_graphics(memory);
 
-    
+    /* Create window */
+    window = SDL_CreateWindow(
+        "Chip-8 Emulator",
+        640,
+        320,
+        SDL_WINDOW_OPENGL
+    );
+    if(window == NULL) {
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
 
+    /* Main loop */
+    while(1) {
+        SDL_Event event;
+        if(SDL_PollEvent(&event)) {
+            if(event.type == SDL_EVENT_QUIT) {
+                break;
+            }
+        }
+    }
+
+
+    /* Destroy window */
+    SDL_DestroyWindow(window);
+
+    /* Free memory */
+    free(memory);
+    free(registers);
+    free(stack);
+    free(display);
+
+    /* Quit SDL */
+    SDL_Quit();
+    return 0;
 }
